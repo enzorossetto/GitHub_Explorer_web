19e70a54a75dda3f54156e95147cb162f85356b7
Enzo Rossetto <enzojmrossetto@gmail.com>
2020-08-06 16:38:19 -0300

    Finished base structure of homepage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -21,6 +22,36 @@ const Dashboard: React.FC = () => {
             src='https://avatars1.githubusercontent.com/u/46872902?s=460&u=7e7ec75c273f6efea8dd8daea334867705651c25&v=4'
             alt='Enzo Rossetto'
           />
+          <div>
+            <strong>enzorossetto/GoBarber_backend_with_NodeJS</strong>
+            <p>Server for GoBarber - Find a barber shop near you</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href='teste'>
+          <img
+            src='https://avatars1.githubusercontent.com/u/46872902?s=460&u=7e7ec75c273f6efea8dd8daea334867705651c25&v=4'
+            alt='Enzo Rossetto'
+          />
+          <div>
+            <strong>enzorossetto/GoBarber_backend_with_NodeJS</strong>
+            <p>Server for GoBarber - Find a barber shop near you</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href='teste'>
+          <img
+            src='https://avatars1.githubusercontent.com/u/46872902?s=460&u=7e7ec75c273f6efea8dd8daea334867705651c25&v=4'
+            alt='Enzo Rossetto'
+          />
+          <div>
+            <strong>enzorossetto/GoBarber_backend_with_NodeJS</strong>
+            <p>Server for GoBarber - Find a barber shop near you</p>
+          </div>
+
+          <FiChevronRight size={20} />
         </a>
       </Repositories>
     </>
