4b6a6c37531dbff6d9b20da69ea9a973de4c64b8
Enzo Rossetto <enzojmrossetto@gmail.com>
2020-08-06 15:31:50 -0300

    Added base structure and styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <h1>Explore repositórios no GitHub</h1>;
+  return (
+    <>
+      <img src={logoImg} alt='GitHub Explorer' />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input placeholder='Digite o nome do repositório' />
+        <button type='submit'>Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href='teste'>
+          <img
+            src='https://avatars1.githubusercontent.com/u/46872902?s=460&u=7e7ec75c273f6efea8dd8daea334867705651c25&v=4'
+            alt='Enzo Rossetto'
+          />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
